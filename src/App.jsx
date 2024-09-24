542ee48f53de2db48ac5204a2d7ce0601faea3c3
Atilas <vampire91145@gmail.com>
2024-09-24 21:30:26 +0800

    Fecthing Orders

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import { loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -42,6 +43,8 @@ function App() {
         {
           path: "/order/:orderId",
           element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />,
         },
       ],
     },
