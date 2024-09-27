44919ddf90168deff674a0ba46d8fc9b85ef6f38
Atilas <vampire91145@gmail.com>
2024-09-28 00:40:00 +0800

    Customizing the Tailwind Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // Place the customize setting in 'theme', means to override the Tailwind default config
   theme: {
-    extend: {},
+    fontFamily: {
+      // Override the sans font that Tailwind automatically injects into our HTML.Therefore, if we change this here then this will automatically update for the entire page.
+      sans: "Roboto Mono, monospace",
+    },
+    // Place in 'extend', means to keep the Tailwind default config, but adding or replacing the specific one to our customize setting.
+    extend: {
+      fontSize: {
+        // Add new defined name that not exist in Tailwind default config --> 'font-huge'
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        // Replace the Tailwind default config : 100vh -> 100dvh
+        screen: "100dvh",
+        // 'vh' creates problems on mobile browsers since sometimes the viewport is not really 100%, so instead of using this unit, we should use more modren unit => 'dvh'
+      },
+    },
   },
   plugins: [],
 };
